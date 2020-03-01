ea3507bee61c9077493d80483b57fee8d8f3d458
Klonan <11986037+Klonan@users.noreply.github.com>
2020-03-01 23:10:10 +0100

    quick script for tile placement not wrecking factories

--- data/tiles/road_tile.lua
+++ data/tiles/road_tile.lua
@@ -4,4 +4,48 @@ tile.name = "transport-drone-road"
 tile.tint = {0.5, 0.5, 0.5}
 tile.collision_mask = shared.tile_collision_mask
 
-data:extend{tile}
+local proxy_tile = util.copy(data.raw.tile["stone-path"])
+
+proxy_tile.name = "transport-drone-proxy-tile"
+proxy_tile.tint = {0.5, 0.5, 0.5}
+
+local proxy_entity = 
+{
+  type = "simple-entity",
+  name = "road-tile-collision-proxy",
+  icon = "__base__/graphics/icons/ship-wreck/small-ship-wreck.png",
+  icon_size = 64, icon_mipmaps = 4,
+  minable = {mining_time = 0.5, result = "iron-gear-wheel"},
+  flags = {"placeable-neutral", "placeable-off-grid", "not-on-map"},
+  subgroup = "wrecks",
+  order = "d[remnants]-d[ship-wreck]-c[small]-a",
+  max_health = 1,
+  collision_box = {{-0.49, -0.49}, {0.49, 0.49}},
+  pictures = util.empty_sprite(),
+  render_layer = "object"
+}
+
+local item = 
+{
+  type = "item",
+  name = "road",
+  icon = "__base__/graphics/icons/concrete.png",
+  icon_size = 64, icon_mipmaps = 4,
+  subgroup = "terrain",
+  order = "b[concrete]-a[plain]",
+  stack_size = 100,
+  place_as_tile =
+  {
+    result = "transport-drone-proxy-tile",
+    condition_size = 1,
+    condition = { "water-tile", "object-layer" }
+  }
+}
+
+data:extend
+{
+  tile,
+  proxy_tile,
+  proxy_entity,
+  item,
+}
---
+++ script/proxy_tile.lua
@@ -0,0 +1,60 @@
+--lets not wallop player things
+
+local real_name = "transport-drone-road"
+
+local on_built_tile = function(event)
+  local tile = event.tile
+  if tile.name ~= "transport-drone-proxy-tile" then return end
+
+  
+  local tiles = event.tiles
+  local surface = game.get_surface(event.surface_index)
+  local refund_count = 0
+  local new_tiles = {}
+
+  for k, tile in pairs (tiles) do
+    local position = tile.position
+    if surface.can_place_entity(
+      {
+        name = "road-tile-collision-proxy",
+        position = {position.x + 0.5, position.y + 0.5},
+        build_check_type = defines.build_check_type.manual
+      }
+    ) then
+      new_tiles[k] = {name = real_name, position = position}
+    else
+      new_tiles[k] = {name = tile.old_tile.name, position = position}
+      refund_count = refund_count + 1
+    end
+  end
+
+  surface.set_tiles(new_tiles)
+
+  if event.item then
+
+    if refund_count > 0 then
+      if event.player then
+        local player = game.get_player(event.player_index)
+        if player then
+          player.insert({name = event.item.name, count = refund_count})
+        end
+      end
+      local robot = event.robot
+      if robot then
+        robot.get_inventory(defines.inventory.robot_cargo).insert({name = event.item.name, count = refund_count})
+      end
+    end
+
+  end
+
+end
+
+local lib = {}
+
+lib.events = 
+{
+  [defines.events.on_player_built_tile] = on_built_tile,
+  [defines.events.on_robot_built_tile] = on_built_tile,
+}
+
+return lib
