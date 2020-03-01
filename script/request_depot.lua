69554f059aaf14ce60207284777b33b49f8d3a8d
Klonan <11986037+Klonan@users.noreply.github.com>
2020-03-01 19:33:31 +0100

    basic scripting in place

--- script/request_depot.lua
+++ script/request_depot.lua
@@ -1,7 +1,13 @@
+
+
 local script_data = 
 {
-  supply_depots = {}
+  request_depots = {},
+  item_map = {}
 }
+
+local request_depot = {}
+local depot_metatable = {__index = request_depot}
 
 local corpse_offsets = 
 {
@@ -11,20 +17,60 @@ local corpse_offsets =
   [6] = {-2, 0},
 }
 
-local request_depot_built = function(entity)
+function request_depot.new(entity)
+
   local position = entity.position
   local direction = entity.direction
   local force = entity.force
   local surface = entity.surface
   local offset = corpse_offsets[direction]
-  
+
   entity.destroy()
 
   local machine = surface.create_entity{name = "request-depot-machine", position = position, force = force}
   machine.active = false
   local corpse = surface.create_entity{name = "caution-corpse", position = {position.x + offset[1], position.y + offset[2]}}
   corpse.corpse_expires = false
-  script_data.supply_depots[machine.unit_number] = {machine = machine, corpse = corpse}
+  
+  local depot =
+  {
+    entity = machine,
+    corpse = corpse,
+    index = tostring(machine.unit_number)
+  }
+  setmetatable(depot, depot_metatable)
+
+  script_data.request_depots[depot.index] = depot
+
+  return depot
+
+end
+
+function request_depot:check_request_change()
+  local requested_item = self:get_requested_item()
+  if self.item == requested_item then return end
+
+  if self.item then
+    script_data.item_map[self.item][self.index] = nil
+    --cancel shit...
+  end
+
+  self.item = requested_item
+
+  if not self.item then return end
+
+  if not script_data.item_map[self.item] then
+    script_data.item_map[self.item] = {}
+  end
+
+  script_data.item_map[self.item][self.index] = self
+
+end
+
+function request_depot:get_requested_item()
+  local recipe = self.entity.get_recipe()
+  if not recipe then return end
+  return recipe.products[1].name
 end
 
 
@@ -34,7 +80,14 @@ local on_created_entity = function(event)
 
   if entity.name ~= "request-transport-depot" then return end
 
-  request_depot_built(entity)
+  request_depot.new(entity)
+
+end
+
+local check_request_change = function(event)
+  for k, request_depot in pairs (script_data.request_depots) do
+    request_depot:check_request_change()
+  end
 end
 
 local lib = {}
@@ -44,12 +97,25 @@ lib.events =
   [defines.events.on_built_entity] = on_created_entity
 }
 
+lib.on_nth_tick =
+{
+  [237] = check_request_change
+}
+
+
 lib.on_init = function()
   global.request_depots = global.request_depots or script_data
 end
 
 lib.on_load = function()
   script_data = global.request_depots or script_data
+  for k, depot in pairs (script_data.request_depots) do
+    setmetatable(depot, depot_metatable)
+  end
+end
+
+lib.get_depots_for_item = function(item)
+  return script_data.item_map[item]
 end
 
 return lib
--- script/supply_depot.lua
+++ script/supply_depot.lua
@@ -1,3 +1,5 @@
+local request_depot = require("script/request_depot")
+
 local script_data = 
 {
   supply_depots = {}
@@ -11,7 +13,10 @@ local corpse_offsets =
   [6] = {-2, 0},
 }
 
-local supply_depot_built = function(entity)
+local supply_depot = {}
+local depot_metatable = {__index = supply_depot}
+
+function supply_depot.new(entity)
   local position = entity.position
   local direction = entity.direction
   local force = entity.force
@@ -21,9 +26,25 @@ local supply_depot_built = function(entity)
   local chest = surface.create_entity{name = "supply-depot-chest", position = position, force = force}
   local corpse = surface.create_entity{name = "caution-corpse", position = {position.x + offset[1], position.y + offset[2]}}
   corpse.corpse_expires = false
-  script_data.supply_depots[chest.unit_number] = {chest = chest, corpse = corpse}
+
+  local depot =
+  {
+    entity = chest,
+    corpse = corpse,
+    index = tostring(chest.unit_number)
+  }
+  setmetatable(depot, depot_metatable)
+
+  script_data.supply_depots[depot.index] = depot
 end
 
+function supply_depot:update()
+  self:say("oh hi :)")
+end
+
+function supply_depot:say(string)
+  self.entity.surface.create_entity{name = "flying-text", position = self.entity.position, text = string}
+end
 
 local on_created_entity = function(event)
   local entity = event.entity or event.created_entity
@@ -31,7 +52,13 @@ local on_created_entity = function(event)
 
   if entity.name ~= "supply-transport-depot" then return end
 
-  supply_depot_built(entity)
+  supply_depot.new(entity)
+end
+
+local update_depots = function(event)
+  for k, depot in pairs (script_data.supply_depots) do
+    depot:update()
+  end
 end
 
 local lib = {}
@@ -41,12 +68,20 @@ lib.events =
   [defines.events.on_built_entity] = on_created_entity
 }
 
+lib.on_nth_tick =
+{
+  [60] = update_depots
+}
+
 lib.on_init = function()
   global.supply_depots = global.supply_depots or script_data
 end
 
 lib.on_load = function()
   script_data = global.supply_depots or script_data
+  for k, depot in pairs (script_data.supply_depots) do
+    setmetatable(depot, depot_metatable)
+  end
 end
 
 return lib
