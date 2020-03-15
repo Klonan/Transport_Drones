1e7a3932651d7d3cdebc1c4486c7a522d5b0c910
Klonan <11986037+Klonan@users.noreply.github.com>
2020-03-15 14:55:12 +0100

    cleanup

--- data.lua
+++ data.lua
@@ -2,10 +2,8 @@ util = require "data/tf_util/tf_util"
 names = require("shared")
 shared = require("shared")
 
-
 require "data/entities/entities"
 require "data/tiles/road_tile"
 require "data/technologies/transport_speed"
 require "data/technologies/transport_capacity"
 require "data/technologies/transport_system"
---require "data/technologies/mining_productivity"
--- data/entities/entities.lua
+++ data/entities/entities.lua
@@ -2,7 +2,3 @@ local require = function(name) return require("data/entities/"..name) end
 
 require("transport_drone/transport_drone")
 require("transport_depot/transport_depot")
-
---require("mining_drone/mining_drone")
---require("proxy_chest/proxy_chest")
---require("mining_depot/mining_depot")
