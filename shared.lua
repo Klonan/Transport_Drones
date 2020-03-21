0bd6fc5689c7379ce8639e52937f30894408136a
Klonan <11986037+Klonan@users.noreply.github.com>
2020-03-21 20:47:31 +0100

    idk

--- shared.lua
+++ shared.lua
@@ -10,8 +10,8 @@ data.transport_speed_technology = "transport-drone-speed"
 data.transport_capacity_technology = "transport-drone-capacity"
 data.transport_system_technology = "transport-system"
 
-data.fuel_amount_per_drone = 10
-data.fuel_consumption_per_meter = 1 / 500
+data.fuel_amount_per_drone = 50
+data.fuel_consumption_per_meter = 1 / 100
 data.drone_fluid_capacity = 1000
 
 return data
