5b3f0ada04462f99228b02a44bde1560d6ca6447
Richard Clamp <richardc@unixbeard.net>
2020-04-02 13:36:31 +0100

    Hack together a basic gui
    This is mostly just copied over wholesale from Teleport.
    
    TODO:
    
    * Text lines of descripton under the maps
    * Localised names of the units
    * Toggle the shortcut properly on window close

--- script/gui.lua
+++ script/gui.lua
@@ -3,10 +3,12 @@ local data =
 {
   gui_frames = {},
   button_actions = {},
+  search_boxes = {},
 }
 
+local preview_size = 160
 
-local debug_print = false
+local debug_print = true
 local print = function(string)
   if not debug_print then return end
   game.print(string)
@@ -25,13 +27,13 @@ local close_gui = function(frame)
   frame.destroy()
 end
 
-local get_gui_frame = function(frame)
+local get_gui_frame = function(player)
   local frame = data.gui_frames[player.index]
   if frame and frame.valid then return frame end
   data.gui_frames[player.index] = nil
 end
 
-local make_gui = function(player, source)
+local make_gui = function(player)
   local location
   local gui_frame = get_gui_frame(player)
   if gui_frame then
@@ -42,11 +44,6 @@ local make_gui = function(player, source)
   end
   
   print("Making new frame")
-  
-  if not (source and source.valid and not data.to_be_removed[source.unit_number]) then
-    --unlink_teleporter(player)
-    return
-  end
   
   local gui = player.gui.screen
   local frame = gui.add{type = "frame", direction = "vertical", ignored_by_interaction = false}
@@ -63,15 +60,15 @@ local make_gui = function(player, source)
   title_flow.style.vertical_align = "center"
   local title = title_flow.add{type = "label", style = "heading_1_label"}
   title.drag_target = frame
-  local rename_button = title_flow.add{type = "sprite-button", sprite = "utility/rename_icon_small", style = "small_picture_slot_button", visible = source.force == player.force}
+  -- local rename_button = title_flow.add{type = "sprite-button", sprite = "utility/rename_icon_small", style = "small_picture_slot_button", visible = source.force == player.force}
   local pusher = title_flow.add{type = "empty-widget", direction = "horizontal", style = "draggable_space_header"}
   pusher.style.horizontally_stretchable = true
   pusher.style.vertically_stretchable = true
   pusher.drag_target = frame
-  -- local search_box = title_flow.add{type = "textfield", visible = false}
-  -- local search_button = title_flow.add{type = "sprite-button", style = "tool_button", sprite = "utility/search_icon", tooltip = {"gui.search-with-focus", {"search"}}}
-  -- util.register_gui(data.button_actions, search_button, {type = "search_button", box = search_box})
-  -- data.search_boxes[player.index] = search_box
+  local search_box = title_flow.add{type = "textfield", visible = false}
+  local search_button = title_flow.add{type = "sprite-button", style = "tool_button", sprite = "utility/search_icon", tooltip = {"gui.search-with-focus", {"search"}}}
+  util.register_gui(data.button_actions, search_button, {type = "search_button", box = search_box})
+  data.search_boxes[player.index] = search_box
   local inner = frame.add{type = "frame", style = "inside_deep_frame"}
   local scroll = inner.add{type = "scroll-pane", direction = "vertical"}
   scroll.style.maximal_height = player.display_resolution.height * 0.8
@@ -80,55 +77,75 @@ local make_gui = function(player, source)
   table.style.horizontal_spacing = 2
   table.style.vertical_spacing = 2
   local any = false
-  --print(table_size(network))
+
   local chart = player.force.chart
-  --for name,  in spairs (network, get_sort_function()) do
-  --   local teleporter_entity = teleporter.teleporter
-  --   if not (teleporter_entity.valid) then
-  --     clear_teleporter_data(teleporter)
-  --   elseif teleporter_entity == source then
-  --     title.caption = name
-  --     util.register_gui(data.button_actions, rename_button, {type = "rename_button", caption = name})
-  --   else
-  --     local position = teleporter_entity.position
-  --     local area = {{position.x - preview_size / 2, position.y - preview_size / 2}, {position.x + preview_size / 2, position.y + preview_size / 2}}
-  --     chart(teleporter_entity.surface, area)
-  --     local button = table.add{type = "button", name = "_"..name}
-  --     button.style.height = preview_size + 32 + 8
-  --     button.style.width = preview_size + 8
-  --     button.style.left_padding = 0
-  --     button.style.right_padding = 0
-  --     local inner_flow = button.add{type = "flow", direction = "vertical", ignored_by_interaction = true}
-  --     inner_flow.style.vertically_stretchable = true
-  --     inner_flow.style.horizontally_stretchable = true
-  --     inner_flow.style.horizontal_align = "center"
-  --     local map = inner_flow.add
-  --     {
-  --       type = "minimap",
-  --       surface_index = teleporter_entity.surface.index,
-  --       zoom = 1,
-  --       force = teleporter_entity.force.name,
-  --       position = position,
-  --     }
-  --     map.ignored_by_interaction = true
-  --     map.style.height = preview_size
-  --     map.style.width = preview_size
-  --     map.style.horizontally_stretchable = true
-  --     map.style.vertically_stretchable = true
-  --     local label = inner_flow.add{type = "label", caption = name}
-  --     label.style.horizontally_stretchable = true
-  --     label.style.font = "default-dialog-button"
-  --     label.style.font_color = {}
-  --     label.style.horizontally_stretchable = true
-  --     label.style.maximal_width = preview_size
-  --     util.register_gui(data.button_actions, button, {type = "teleport_button", param = teleporter})
-  --     any = true
-  --   end
-  -- end
-  if not any then
-      table.add{type = "label", caption = {"no-transport-depots"}}
+
+  for i, depot in pairs(depot.get_all_depots()) do
+    any = true
+    print(serpent.line(depot))
+    name = depot.entity.name..i
+    depot_entity = depot.entity
+    local position = depot_entity.position
+    local area = {{position.x - preview_size / 2, position.y - preview_size / 2}, {position.x + preview_size / 2, position.y + preview_size / 2}}
+    chart(depot_entity.surface, area)
+
+    local button = table.add{type = "button", name = "_"..name}
+    button.style.height = preview_size + 32 + 8
+    button.style.width = preview_size + 8
+    button.style.left_padding = 0
+    button.style.right_padding = 0
+    local inner_flow = button.add{type = "flow", direction = "vertical", ignored_by_interaction = true}
+    inner_flow.style.vertically_stretchable = true
+    inner_flow.style.horizontally_stretchable = true
+    inner_flow.style.horizontal_align = "center"
+
+    local map = inner_flow.add
+    {
+      type = "minimap",
+      surface_index = depot_entity.surface.index,
+      zoom = 1,
+      force = depot_entity.force.name,
+      position = position,
+    }
+    map.ignored_by_interaction = true
+    map.style.height = preview_size
+    map.style.width = preview_size
+    map.style.horizontally_stretchable = true
+    map.style.vertically_stretchable = true
+
+    local label = inner_flow.add{type = "label", caption = name}
+    label.style.horizontally_stretchable = true
+    label.style.font = "default-dialog-button"
+    label.style.font_color = {}
+    label.style.horizontally_stretchable = true
+    label.style.maximal_width = preview_size
+    util.register_gui(data.button_actions, button, {type = "depot_button", param = depot})
   end
-end  
+
+  if not any then
+    table.add{type = "label", caption = {"no-transport-depots"}}
+  end
+end
+
+local gui_actions =
+{
+  search_text_changed = function(event, param)
+    local box = event.element
+    local search = box.text
+    local parent = param.parent
+    for k, child in pairs (parent.children) do
+      child.visible = child.name:lower():find(search:lower(), 1, true)
+    end
+  end,
+  search_button = function(event, param)
+    param.box.visible = not param.box.visible
+    if param.box.visible then param.box.focus() end
+  end,
+  depot_button = function(event, param)
+    print("BUTTON: "..serpent.line(event).." "..serpent.line(param))
+  end,
+}
+
 
 local on_gui_action = function(event)
   local element = event.element
@@ -162,9 +179,18 @@ local on_player_removed = function(event)
   close_gui(get_gui_frame(player))
 end
 
+local on_lua_shortcut = function(event)
+  if event.prototype_name ~= "transport-drones-gui" then return end
+
+  print("TOGGLE GUI")
+  local player = game.players[event.player_index]
+  make_gui(player, nil)
+end
 
 local lib = {}
 lib.events = {
+  [defines.events.on_lua_shortcut] = on_lua_shortcut,
+
   [defines.events.on_gui_click] = on_gui_action,
   [defines.events.on_gui_text_changed] = on_gui_action,
   [defines.events.on_gui_confirmed] = on_gui_action,
