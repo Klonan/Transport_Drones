ca5d25b1127b4776f7bcc68d89cef5ed5a090d4b
Klonan <11986037+Klonan@users.noreply.github.com>
2020-04-04 20:44:10 +0200

    Add remote in body of the script

--- control.lua
+++ control.lua
@@ -10,4 +10,5 @@ handler.add_lib(require("script/transport_drone"))
 handler.add_lib(require("script/proxy_tile"))
 handler.add_lib(require("script/blueprint_correction"))
 handler.add_lib(require("script/transport_technologies"))
-handler.add_lib(require("script/remote_interface"))
+
+require("script/remote_interface")
--- script/remote_interface.lua
+++ script/remote_interface.lua
@@ -11,12 +11,6 @@ local interface =
   end
 }
 
-local lib = {}
-
-lib.add_remote_interface = function()
-  if not remote.interfaces["transport_drones"] then
-    remote.add_interface("transport_drones", interface)
-  end
+if not remote.interfaces["transport_drones"] then
+  remote.add_interface("transport_drones", interface)
 end
-
-return lib
