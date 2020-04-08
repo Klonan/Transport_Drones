bb67b9b3eaa78744e3d0e290933e64ad7d6fb709
Klonan <11986037+Klonan@users.noreply.github.com>
2020-04-08 12:18:23 +0200

    Fix rail and gate collision masks with mods

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,17 +6,29 @@ local rail_collision_mask = {"floor-layer", "water-tile", "item-layer"}
 local gate_collision_mask = {"item-layer", "player-layer", "train-layer", "water-tile"}
 
 for k, rail in pairs (data.raw["straight-rail"]) do
-  rail.collision_mask = rail.collision_mask or rail_collision_mask
+  if rail.collision_mask then
+    util.remove_from_list(rail.collision_mask, "object-layer")
+  else
+    rail.collision_mask = rail_collision_mask
+  end
   table.insert(rail.collision_mask, "layer-14")
   rail.selection_priority = 45
 end
 
 for k, rail in pairs (data.raw["curved-rail"]) do
-  rail.collision_mask = rail.collision_mask or rail_collision_mask
+  if rail.collision_mask then
+    util.remove_from_list(rail.collision_mask, "object-layer")
+  else
+    rail.collision_mask = rail_collision_mask
+  end
   table.insert(rail.collision_mask, "layer-14")
   rail.selection_priority = 45
 end
 
 for k, gate in pairs (data.raw.gate) do
-  gate.collision_mask = gate.collision_mask or gate_collision_mask
+  if gate.collision_mask then
+    util.remove_from_list(gate.collision_mask, "object-layer")
+  else
+    gate.collision_mask = gate_collision_mask
+  end
 end
