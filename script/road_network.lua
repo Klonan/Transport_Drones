2c512be244034fadc811e0a9654643c310e96d7d
Klonan <11986037+Klonan@users.noreply.github.com>
2020-04-11 20:07:53 +0200

    Oh, more robust network splitting merging logic.

--- script/road_network.lua
+++ script/road_network.lua
@@ -5,6 +5,11 @@ local script_data =
   node_map = {}
 }
 
+local print = function(string)
+  log(string)
+  game.print(string)
+end
+
 local new_id = function()
   script_data.id_number = script_data.id_number + 1
   local id = script_data.id_number
@@ -12,7 +17,7 @@ local new_id = function()
   {
     id = id
   }
-  --game.print("New network "..id)
+  --print("New network "..id)
   return id
 end
 
@@ -98,14 +103,23 @@ local accumulate_nodes = function(surface, x, y)
 
 end
 
-local connection_check = function(surface, x, y, target_node)
+local symmetric_connection_check = function(surface, x1, y1, x2, y2)
+  --Because most often, 1 road network is significantly smaller, so this will reduce search time.
 
-  local nodes = {}
-  local new_nodes = {}
-
-  local root_node = get_node(surface, x, y)
-  nodes[root_node] = true
-  new_nodes[root_node] = {x, y}
+  local nodes_1 = {}
+  local new_nodes_1 = {}
+  
+  local root_node_1 = get_node(surface, x1, y1)
+  nodes_1[root_node_1] = true
+  new_nodes_1[root_node_1] = {x1, y1}
+  
+  local nodes_2 = {}
+  local new_nodes_2 = {}
+  
+  local root_node_2 = get_node(surface, x2, y2)
+  nodes_2[root_node_2] = true
+  new_nodes_2[root_node_2] = {x2, y2}
+  
 
   local neighbor_offsets = neighbor_offsets
   local get_node = get_node
@@ -113,34 +127,130 @@ local connection_check = function(surface, x, y, target_node)
   local pairs = pairs
 
   while true do
-    local node, node_position = next(new_nodes)
+
+    local node, node_position = next(new_nodes_1)
     if not node then break end
-    new_nodes[node] = nil
+    --game.surfaces[surface].create_entity{name = "flying-text", position = {node_position[1], node_position[2]}, text = "A"}
+    new_nodes_1[node] = nil
     for k, offset in pairs (neighbor_offsets) do
       local nx, ny = node_position[1] + offset[1], node_position[2] + offset[2]
       local neighbor = get_node(surface, nx, ny)
-      
       if neighbor then
-        if neighbor == target_node then return true end
-        if not nodes[neighbor] then
-          nodes[neighbor] = true
-          new_nodes[neighbor] = {nx, ny}
+
+        if nodes_2[neighbor] then return true end
+
+        if not nodes_1[neighbor] then
+          nodes_1[neighbor] = true
+          new_nodes_1[neighbor] = {nx, ny}
         end
       end
     end
+    
+    local node, node_position = next(new_nodes_2)
+    if not node then break end
+    --game.surfaces[surface].create_entity{name = "flying-text", position = {node_position[1], node_position[2]}, text = "B"}
+    new_nodes_2[node] = nil
+    for k, offset in pairs (neighbor_offsets) do
+      local nx, ny = node_position[1] + offset[1], node_position[2] + offset[2]
+      local neighbor = get_node(surface, nx, ny)
+      if neighbor then
+
+        if nodes_1[neighbor] then return true end
+
+        if not nodes_2[neighbor] then
+          nodes_2[neighbor] = true
+          new_nodes_2[neighbor] = {nx, ny}
+        end
+      end
+    end
+
   end
 
   return false 
   
 end
 
-local set_node_ids = function(surface, x, y, id)
+local accumulate_smaller_node = function(surface, x1, y1, x2, y2)
+
+  --returns the smaller of the 2 node groups.
   
-  local nodes = accumulate_nodes(surface, x, y)
+  local nodes_1 = {}
+  local new_nodes_1 = {}
+  
+  local root_node_1 = get_node(surface, x1, y1)
+  nodes_1[root_node_1] = true
+  new_nodes_1[root_node_1] = {x1, y1}
+  
+  local nodes_2 = {}
+  local new_nodes_2 = {}
+  
+  local root_node_2 = get_node(surface, x2, y2)
+  nodes_2[root_node_2] = true
+  new_nodes_2[root_node_2] = {x2, y2}
+  
+
+  local neighbor_offsets = neighbor_offsets
+  local get_node = get_node
+  local next = next
+  local pairs = pairs
+
+  while true do
+
+    local node, node_position = next(new_nodes_1)
+    if not node then return nodes_1 end
+    --game.surfaces[surface].create_entity{name = "flying-text", position = {node_position[1], node_position[2]}, text = "A"}
+    new_nodes_1[node] = nil
+    for k, offset in pairs (neighbor_offsets) do
+      local nx, ny = node_position[1] + offset[1], node_position[2] + offset[2]
+      local neighbor = get_node(surface, nx, ny)
+      if neighbor then
+        if not nodes_1[neighbor] then
+          nodes_1[neighbor] = true
+          new_nodes_1[neighbor] = {nx, ny}
+        end
+      end
+    end
+    
+    local node, node_position = next(new_nodes_2)
+    if not node then return nodes_2 end
+    --game.surfaces[surface].create_entity{name = "flying-text", position = {node_position[1], node_position[2]}, text = "B"}
+    new_nodes_2[node] = nil
+    for k, offset in pairs (neighbor_offsets) do
+      local nx, ny = node_position[1] + offset[1], node_position[2] + offset[2]
+      local neighbor = get_node(surface, nx, ny)
+      if neighbor then
+        if not nodes_2[neighbor] then
+          nodes_2[neighbor] = true
+          new_nodes_2[neighbor] = {nx, ny}
+        end
+      end
+    end
+
+  end
+
+end
+
+local debug_get_node_postion = function(node)
+  for surface, v in pairs (script_data.node_map) do
+    for x, y in pairs (v) do
+      for y, j in pairs (y) do
+        if j == node then
+          return {surface = surface, x = x, y = y}
+        end
+      end
+    end
+  end
+  return {1, 1, 1}
+end
+
+local set_node_ids = function(nodes, id)
+
+  --print("Setting nodes "..id)
 
   for node, bool in pairs (nodes) do
     node.id = id
-    --game.surfaces[surface].create_entity{name = "flying-text", position = {x, y}, text = id}
+    --local node_position = debug_get_node_postion(node)
+    --game.surfaces[node_position.surface].create_entity{name = "flying-text", position = {node_position.x, node_position.y}, text = id}
 
     if node.depots then
       for k, depot in pairs (node.depots) do
@@ -152,7 +262,10 @@ local set_node_ids = function(surface, x, y, id)
 
 end
 
+
+
 local clear_network = function(id)
+  --print("Clearing "..id)
   local network = script_data.networks[id]
 
   for k, name in pairs ({"supply", "mining", "fuel"}) do
@@ -190,6 +303,68 @@ local road_network = {}
 
 road_network.add_node = function(surface, x, y)
 
+  local prospective_id
+  local prospective_position
+
+  for k, offset in pairs (neighbor_offsets) do
+    local nx, ny = x + offset[1], y + offset[2]
+    local node = get_node(surface, nx, ny)
+    if node then
+      if not prospective_id then
+        prospective_id = node.id
+        prospective_position = {nx, ny}
+      else
+        if node.id ~= prospective_id then
+          --merging networks
+          local smaller_node_set = accumulate_smaller_node(surface, prospective_position[1], prospective_position[2], nx, ny)
+          local smaller_id = next(smaller_node_set).id
+          if smaller_id == prospective_id then
+            prospective_id = node.id
+            prospective_position = {nx, ny}
+          end
+          set_node_ids(smaller_node_set, prospective_id)
+          clear_network(smaller_id)
+        end
+      end
+    end
+  end
+
+
+  local new_node_id
+  local checked = {}
+  for k, offset in pairs(neighbor_offsets) do
+
+    checked[k] = true
+    
+    local fx, fy = x + offset[1], y + offset[2]
+    local neighbor = get_node(surface, fx, fy)
+    
+    if neighbor then
+
+      if not new_node_id then new_node_id = neighbor.id end
+
+      for j, offset in pairs(neighbor_offsets) do
+        if not checked[j] then
+
+          local nx, ny = x + offset[1], y + offset[2]
+          local other_neighbor = get_node(surface, nx, ny)
+          if other_neighbor then
+            if neighbor.id ~= other_neighbor.id then
+              local smaller_node_set = accumulate_smaller_node(surface, fx, fy, nx, ny)
+              local smaller_id = next(smaller_node_set).id
+              local larger_id = smaller_id == neighbor.id and other_neighbor.id or neighbor.id
+              set_node_ids(smaller_node_set, larger_id) 
+              new_node_id = larger_id
+            end
+          end
+
+        end
+      end
+    end
+
+  end
+  
+
   local surface_map = script_data.node_map[surface]
   if not surface_map then
     surface_map = {}
@@ -202,55 +377,25 @@ road_network.add_node = function(surface, x, y)
     surface_map[x] = x_map
   end
   
-  local node =
+  if not new_node_id then
+    new_node_id = new_id()
+  end
+  
+  x_map[y] = 
   {
-    id = 0
+    id = new_node_id
   }
-  x_map[y] = node
-
-  
-  local prospective_id
-  local merge_networks = false
-  local neighbors = get_neighbors(surface, x, y)
-  for k, node in pairs (neighbors) do
-    if not prospective_id then
-      prospective_id = node.id
-    elseif node.id ~= prospective_id then
-      prospective_id = math.min(node.id, prospective_id)
-      --will need to merge network
-      merge_networks = true
-    end
-  end
-  
-  if not prospective_id then
-    prospective_id = new_id()
-  end
-
-  node.id = prospective_id
-
-  if merge_networks then
-    for k, offset in pairs (neighbor_offsets) do
-      local nx, ny = x + offset[1], y + offset[2]
-      local node = get_node(surface, nx, ny)
-      if node then
-        local old_id = node.id
-        if old_id ~= prospective_id then
-          set_node_ids(surface, nx, ny, prospective_id)
-          clear_network(old_id)
-        end
-      end
-    end
-  end
-
-  --game.print(table_size(script_data.networks))
 
 end
 
 road_network.remove_node = function(surface, x, y)
 
+  
   local node = get_node(surface, x, y)
   if not node then return end
   
+  --print("Removing node "..serpent.line({node.id, x, y}))
+
   if node.depots and next(node.depots) then
     return true
   end
@@ -262,7 +407,7 @@ road_network.remove_node = function(surface, x, y)
   --game.surfaces[surface].create_entity{name = "flying-text", position = {x, y}, text = count}
 
   if count == 0 then
-    -- No neighbors, clear the network.    
+    -- No neighbors, clear the network.
     clear_network(node.id)
     return
   end
@@ -273,19 +418,30 @@ road_network.remove_node = function(surface, x, y)
   end
 
   -- we could be splitting neighbors.
-  local fx, fy
-  for k, offset in pairs (neighbor_offsets) do
-    local nx, ny = x + offset[1], y + offset[2]
-    local neighbor = get_node(surface, nx, ny)
+  -- Check every neighbor against every other neighbor
+  local checked = {}
+  for k, offset in pairs(neighbor_offsets) do
+
+    checked[k] = true
+    
+    local fx, fy = x + offset[1], y + offset[2]
+    local neighbor = get_node(surface, fx, fy)
+    
     if neighbor then
-      if not fx then
-        fx, fy = nx, ny 
-      else
-        if not (connection_check(surface, fx, fy, neighbor)) then              
-          set_node_ids(surface, nx, ny, new_id()) 
+      for j, offset in pairs(neighbor_offsets) do
+        if not checked[j] then
+          local nx, ny = x + offset[1], y + offset[2]
+          local other_neighbor = get_node(surface, nx, ny)
+          if other_neighbor then
+            if not symmetric_connection_check(surface, fx, fy, nx, ny) then
+              local smaller_node_set = accumulate_smaller_node(surface, fx, fy, nx, ny)
+              set_node_ids(smaller_node_set, new_id()) 
+            end
+          end
         end
       end
     end
+
   end
 
 end
