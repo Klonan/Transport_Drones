c7384c325933519d0662d26f91ccbecd0835ad73
Klonan <11986037+Klonan@users.noreply.github.com>
2020-04-11 11:43:44 +0200

    check tile placement

--- script/blueprint_correction.lua
+++ script/blueprint_correction.lua
@@ -4,6 +4,36 @@ local tile_correction =
   ["transport-drone-road"] = "transport-drone-proxy-tile"
 }
 
+local placement_check = 
+{
+  ["transport-drone-proxy-tile"] = "road-tile-collision-proxy"
+}
+
+local correct_tile_ghost = function(entity, correct_name)
+
+  local position = entity.position
+  local force = entity.force
+  local surface = entity.surface
+  entity.destroy()
+  surface.create_entity{name = "tile-ghost", inner_name = correct_name, position = position, force = force, surface = surface, raise_built = true}
+
+end
+
+local do_placement_check = function(entity, placement_check)
+
+  if entity.surface.can_place_entity(
+    {
+      name = placement_check,
+      position = entity.position,
+      build_check_type = defines.build_check_type.manual
+    }
+  ) then return
+  end
+
+  entity.destroy()
+
+end
+
 local on_built_entity = function(event)
   local entity = event.created_entity
   if not (entity and entity.valid) then return end
@@ -13,16 +43,18 @@ local on_built_entity = function(event)
   end
 
   local correct_name = tile_correction[entity.ghost_name]
+  if correct_name then
+    correct_tile_ghost(entity, correct_name)
+    return
+  end
 
-  if not correct_name then return end
+  local placement_check = placement_check[entity.ghost_name]
+  if placement_check then
+    do_placement_check(entity, placement_check)
+    return
+  end
 
-  local position = entity.position
-  local force = entity.force
-  local surface = entity.surface
-  
-  entity.destroy()
 
-  surface.create_entity{name = "tile-ghost", inner_name = correct_name, position = position, force = force, surface = surface, raise_built = true}
 
 end
 
